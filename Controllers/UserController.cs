2d3d39a4fa8a99599fa45e4f4638c3934c6876f5
dlrandy <qq1208484996@qq.com>
2024-03-24 10:33:31 +0800

    feat: role operations

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using IdentityManager.Constants;
 using IdentityManager.Data;
 using IdentityManager.Models;
+using IdentityManager.Models.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,11 +12,14 @@ namespace IdentityManager.Controllers
 	{
 		private readonly ApplicationDbContext _db;
 		private readonly UserManager<ApplicationUser> _userManager;
+		private readonly RoleManager<IdentityRole> _roleManager;
 
-		public UserController(ApplicationDbContext db, UserManager<ApplicationUser> userManager) {
+		public UserController(ApplicationDbContext db, UserManager<ApplicationUser> userManager,
+			RoleManager<IdentityRole> roleManager
+			) {
 			_db = db;
 			_userManager = userManager;
-
+			_roleManager = roleManager;
 		}
 
 		public IActionResult Index() {
@@ -38,6 +43,107 @@ namespace IdentityManager.Controllers
 			return View(userList);
 		}
 
-	}
+		public async Task<IActionResult> ManageRole(string userId)
+		{
+			ApplicationUser user = await _userManager.FindByIdAsync(userId);
+			if (user == null)
+			{
+				return NotFound();
+			}
+
+			List<string> existingUserRoels = (List<string>)await _userManager.GetRolesAsync(user);
+			var model = new RolesViewModel()
+			{
+				User = user
+			};
+			foreach (var role in _roleManager.Roles)
+			{
+				RolesSelection roleSelection = new RolesSelection() {
+					RoleName = role.Name
+				};
+				if (existingUserRoels.Any(c => c == role.Name))
+				{
+					roleSelection.IsSelected = true;
+				}
+				model.RolesList.Add(roleSelection);
+			}
+
+			return View(model);
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> ManageRole(RolesViewModel rolesViewModel)
+		{
+			ApplicationUser user = await _userManager.FindByIdAsync(rolesViewModel.User?.Id);
+			if (user == null)
+			{
+				return NotFound();
+			}
+			var oldUserRoles = await _userManager.GetRolesAsync(user);
+			var result = await _userManager.RemoveFromRolesAsync(user, oldUserRoles);
+			if (!result.Succeeded)
+			{
+				TempData[SD.Error] = "Error while removing roles";
+				return View(rolesViewModel);
+			}
+
+			result = await _userManager.AddToRolesAsync(user,
+				rolesViewModel.RolesList.Where(x => x.IsSelected).Select(y => y.RoleName));
+
+			if (!result.Succeeded)
+			{
+				TempData[SD.Error] = "Error while adding roles";
+				return View(rolesViewModel);
+			}
+
+			TempData[SD.Success] = "Roles assigned successfully";
+			return RedirectToAction(nameof(Index));
+		}
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> LockUnlock(string userId)
+        {
+			//ApplicationUser user = await _userManager.FindByIdAsync(userId);
+            ApplicationUser user = _db.ApplicationUser.FirstOrDefault(u => u.Id == userId); // more safe
+            if (user == null)
+            {
+                return NotFound();
+            }
+            if (user.LockoutEnd != null && user.LockoutEnd > DateTime.Now)
+            {
+                //user is locked and will remain locked untill lockoutend time
+                //clicking on this action will unlock them
+                user.LockoutEnd = DateTime.Now;
+                TempData[SD.Success] = "User unlocked successfully";
+            }
+            else
+            {
+                //user is not locked, and we want to lock the user
+                user.LockoutEnd = DateTime.Now.AddYears(1000);
+                TempData[SD.Success] = "User locked successfully";
+            }
+            _db.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> DeleteUser(string userId)
+		{
+			var user = _db.ApplicationUser.FirstOrDefault(u => u.Id == userId);
+			if (user == null)
+			{
+				return NotFound();
+			}
+			_db.ApplicationUser.Remove(user);
+			_db.SaveChanges();
+			TempData[SD.Success] = "User deleted successfully";
+			return RedirectToAction(nameof(Index));
+		}
+
+    }
 }
 
---
+++ Models/ViewModels/RolesViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace IdentityManager.Models.ViewModels
+{
+	public class RolesViewModel
+	{
+		public ApplicationUser User { get; set; }
+		public List<RolesSelection> RolesList { get; set; }
+		public RolesViewModel()
+		{
+			RolesList = new List<RolesSelection>();
+		}
+	}
+
+	public class RolesSelection {
+		public string RoleName { get; set; }
+		public bool IsSelected { get; set; }
+	}
+}
+
